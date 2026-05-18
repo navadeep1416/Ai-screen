765ef6aae3aa4f691171cea5ca29449a02c9c7fa
navadeep1416 <githubeducationpack@gmail.com>
2026-05-18 16:32:05 +0530

    Update overlay_service.dart

--- lib/overlay/overlay_service.dart
+++ lib/overlay/overlay_service.dart
@@ -33,7 +33,7 @@ class OverlayService {
       visibility: NotificationVisibility.visibilityPublic,
       positionGravity: PositionGravity.auto,
       height: AppConstants.overlayMediumHeight.toInt(),
-      width: WindowSize.matchParent,
+      width: WindowSize.matchParent.toInt(),
     );
     startWatching('Crush');
   }
